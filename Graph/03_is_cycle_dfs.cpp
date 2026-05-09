df0c5796b3c95e8114f3a2d179ea91acff2bf7b1
Nayan Panchal <nayanpanchal2007@gmail.com>
2026-05-09 09:21:11 +0530

    Is cycle using dfs

---
+++ Graph/03_is_cycle_dfs.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+class Graph {
+    int V;
+    list<int>* l;
+
+public:
+    Graph(int V) {
+        this->V = V;
+        l = new list<int>[V];
+    }
+
+    ~Graph() {
+        delete[] l;
+    }
+    
+    void add_edge(int u, int v) {
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+
+    void print_list() {
+        for (int i = 0; i < V; i++) {
+            cout << i << " : ";
+            for (int x : l[i]) {
+                cout << x << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    bool is_cycle_undirected_dfs(int src, int parent, vector<bool>& visited) {
+        visited[src] = true;
+
+        for (int v : l[src]) {
+            if (!visited[v]) {
+                if (is_cycle_undirected_dfs(v, src, visited)) {
+                    return true;
+                } 
+            } else if (v != parent) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool is_cycle() {
+        vector<bool> visited(V, false);
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (is_cycle_undirected_dfs(i, -1, visited)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    Graph g(5);
+
+    g.add_edge(0, 1);
+    g.add_edge(0, 2);
+    g.add_edge(0, 3);
+    g.add_edge(1, 2);
+    g.add_edge(3, 4);
+
+    if (g.is_cycle()) {
+        cout << "Cycle Exists." << endl;
+    } else {
+        cout << "Cycle Not Exists." << endl;
+    }
+
+    return 0;
+}
