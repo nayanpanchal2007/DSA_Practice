dd420d4696db0934b0d99871b2965b15c7a87f16
Nayan Panchal <nayanpanchal2007@gmail.com>
2026-05-10 09:55:10 +0530

    Insertion sort

---
+++ Sorting/03_insertion_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertion_sort(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    vector<int> arr = {5, 6, 2, 3, 1, 4};
+
+    insertion_sort(arr);
+
+    for (int x : arr) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
