3a30363edd435f8414827a27d09a6b9aeff24ad5
Nayan Panchal <nayanpanchal2007@gmail.com>
2026-07-30 20:54:55 +0530

    Counting sort

--- Sorting/07_counting_sort.cpp
+++ Sorting/07_counting_sort.cpp
@@ -1,46 +1,35 @@
 #include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> counting_sort(vector<int>& arr) {
-    int n = arr.size();
+void counting_sort(vector<int>& arr) {
+    int mx = *max_element(arr.begin(), arr.end());
 
-    int mx = arr[0];
+    vector<int> freq(mx + 1, 0);
 
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > mx) {
-            mx = arr[i];
+    for (int& x : arr) {
+        freq[x]++;
+    }
+
+    int idx = 0;
+
+    for (int i = 0; i <= mx; i++) {
+        while (freq[i]--) {
+            arr[idx] = i;
+            idx++;
         }
     }
-
-    vector<int> count(mx + 1);
-
-    for (int x : arr) {
-        count[x]++;
-    }
-
-    for (int i = 1; i <= mx; i++) {
-        count[i] += count[i - 1];
-    }
-
-    vector<int> ans(n);
-
-    for (int i = n - 1; i >= 0; i--) {
-        ans[count[arr[i]] - 1] = arr[i];
-        count[arr[i]]--;
-    } 
-    
-    return ans;
 }
 
 int main() {
-    vector<int> arr = {8, 8, 9, 4, 4, 1, 2, 1, 1, 3};
+    vector<int> arr = {5, 3, 2, 1, 4, 8, 7, 9, 6};
 
-    vector<int> result = counting_sort(arr);
-
-    for (int x : result) {
+    counting_sort(arr);
+    
+    for (int& x : arr) {
         cout << x << " ";
     }
+
     cout << endl;
     return 0;
 }
